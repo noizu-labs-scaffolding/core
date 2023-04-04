a2b3ac419f4397a17924bcef312b6daaabd534c6
Keith Brings <keith.brings@noizu.com>
2023-04-04 08:34:54 +0700

    Initial Commit

---
+++ lib/noizu_labs_erp.ex
@@ -0,0 +1,18 @@
+defmodule NoizuLabsErp do
+  @moduledoc """
+  Documentation for `NoizuLabsErp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NoizuLabsErp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/noizu_labs_erp_test.exs
@@ -0,0 +1,8 @@
+defmodule NoizuLabsErpTest do
+  use ExUnit.Case
+  doctest NoizuLabsErp
+
+  test "greets the world" do
+    assert NoizuLabsErp.hello() == :world
+  end
+end
