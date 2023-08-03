3a2e5953d86767b8732424f78bdc6c0da36adb3b
Keith Brings <keith.brings@noizu.com>
2023-08-03 08:26:19 +0700

    vsn bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Noizu.Core.MixProject do
     [
       app: :noizu_labs_core,
       name: "Noizu Core",
-      version: "0.1.0",
+      version: "0.1.1",
       package: package(),
       description: description(),
       elixir: "~> 1.14",
