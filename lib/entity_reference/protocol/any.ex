f2389327e53bae233b2a5a0fbd783a3e364ccf4a
Keith Brings <keith.brings@noizu.com>
2023-08-06 21:34:13 +0700

    Improved error tupes and typo fix

--- lib/entity_reference/protocol/any.ex
+++ lib/entity_reference/protocol/any.ex
@@ -1,9 +1,9 @@
 defimpl  Noizu.EntityReference.Protocol, for: Any do
-  def id(subject), do: {:error, {:unsupported, subject}}
-  def kind(subject), do: {:error, {:unsupported, subject}}
-  def ref(subject), do: {:error, {:unsupported, subject}}
-  def sref(subject), do: {:error, {:unsupported, subject}}
-  def entity(subject, _), do: {:error, {:unsupported, subject}}
+  def id(subject), do: {:error, {Noizu.EntityReference.Protocol, {:unsupported, {:id, subject}}}}
+  def kind(subject), do: {:error, {Noizu.EntityReference.Protocol, {:unsupported, {:kind, subject}}}}
+  def ref(subject), do: {:error, {Noizu.EntityReference.Protocol, {:unsupported, {:ref, subject}}}}
+  def sref(subject), do: {:error, {Noizu.EntityReference.Protocol, {:unsupported, {:sref, subject}}}}
+  def entity(subject, _), do: {:error, {Noizu.EntityReference.Protocol, {:unsupported, {:entity, subject}}}}
 
   defmacro __deriving__(module, struct, options) do
     deriving(module, struct, options)
@@ -13,7 +13,7 @@ defimpl  Noizu.EntityReference.Protocol, for: Any do
     quote do
       defimpl  Noizu.EntityReference.Protocol, for: [unquote(module)] do
         def id(subject), do: apply(unquote(module), :id, [subject])
-        def kind(subject), do: apply(unquote(module), :id, [subject])
+        def kind(subject), do: apply(unquote(module), :kind, [subject])
         def ref(subject), do: apply(unquote(module), :ref, [subject])
         def sref(subject), do: apply(unquote(module), :sref, [subject])
         def entity(subject, context), do: apply(unquote(module), :entity, [subject, context])
