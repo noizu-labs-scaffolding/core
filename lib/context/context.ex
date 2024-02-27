12576c1726df60ac0231ce9e5132930a7ea6db12
Keith Brings <keith.brings@noizu.com>
2024-02-27 22:29:47 +0700

    updating scaffolding to use id instead of id to reduce impedence with ecte/live-view conventions

--- lib/context/context.ex
+++ lib/context/context.ex
@@ -5,13 +5,13 @@ defmodule Noizu.Context.Entity do
   #-------------------
   #
   #-------------------
-  def id(R.ref(module: Noizu.Context.Entity, identifier: id)), do: {:ok, id}
+  def id(R.ref(module: Noizu.Context.Entity, id: id)), do: {:ok, id}
 
 
   #-------------------
   #
   #-------------------
-  def ref(role) when is_atom(role), do: {:ok, R.ref(module: Noizu.Context.Entity, identifier: role)}
+  def ref(role) when is_atom(role), do: {:ok, R.ref(module: Noizu.Context.Entity, id: role)}
   def ref(R.ref(module: Noizu.Context.Entity) = ref), do: {:ok, ref}
 end
 
@@ -24,16 +24,16 @@ defmodule Noizu.Context do
   #-------------------
   #
   #-------------------
-  defp roles(R.ref(module: Noizu.Context.Entity, identifier: :restricted)) do
+  defp roles(R.ref(module: Noizu.Context.Entity, id: :restricted)) do
     {:ok, [restricted: true]}
   end
-  defp roles(R.ref(module: Noizu.Context.Entity, identifier: :internal)) do
+  defp roles(R.ref(module: Noizu.Context.Entity, id: :internal)) do
     {:ok, [internal: true]}
   end
-  defp roles(R.ref(module: Noizu.Context.Entity, identifier: :system)) do
+  defp roles(R.ref(module: Noizu.Context.Entity, id: :system)) do
     {:ok, [system: true]}
   end
-  defp roles(R.ref(module: Noizu.Context.Entity, identifier: :admin)) do
+  defp roles(R.ref(module: Noizu.Context.Entity, id: :admin)) do
     {:ok, [admin: true]}
   end
 
