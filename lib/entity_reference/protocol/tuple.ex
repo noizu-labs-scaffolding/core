b2516aa49d83c644384b44af5ff680f995797c4d
Keith Brings <keith.brings@noizu.com>
2024-02-28 01:33:09 +0700

    erp tuple impl update

--- lib/entity_reference/protocol/tuple.ex
+++ lib/entity_reference/protocol/tuple.ex
@@ -1,18 +1,26 @@
 defimpl  Noizu.EntityReference.Protocol, for: Tuple do
   require Noizu.EntityReference.Records
   alias Noizu.EntityReference.Records, as: R
+  def id({:error, } = e), do: e
   def id(R.ref(module: h) = subject) do
     apply(h, :id, [subject])
   end
-  def id(R.ref(module: h) = subject) do
+  def kind({:error, } = e), do: e
+  def kind(R.ref(module: h) = subject) do
     apply(h, :kind, [subject])
   end
+
+  def ref({:error, } = e), do: e
   def ref(R.ref(module: h) = subject) do
     apply(h, :ref, [subject])
   end
+
+  def sref({:error, } = e), do: e
   def sref(R.ref(module: h) = subject) do
     apply(h, :sref, [subject])
   end
+
+  def entity({:error, } = e, _), do: e
   def entity(R.ref(module: h) = subject, context) do
     apply(h, :entity, [subject, context])
   end
